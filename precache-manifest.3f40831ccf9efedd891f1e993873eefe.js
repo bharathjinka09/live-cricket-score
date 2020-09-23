df672d72e543f00e5768d7d80d306b84b9b57cbd
bharathjinka09 <bharathjinka09@gmail.com>
2020-09-23 10:37:17 +0530

    Updates

---
+++ precache-manifest.3f40831ccf9efedd891f1e993873eefe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdcf4fee5bd73566e75496e7f35d3c3",
+    "url": "/live-cricket-score/index.html"
+  },
+  {
+    "revision": "91479c2168ac21069ac9",
+    "url": "/live-cricket-score/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99d0adf05a0187eb8a25",
+    "url": "/live-cricket-score/static/js/2.606eff13.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/live-cricket-score/static/js/2.606eff13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91479c2168ac21069ac9",
+    "url": "/live-cricket-score/static/js/main.07f2bffa.chunk.js"
+  },
+  {
+    "revision": "bd0c13206e4ac7d0024a",
+    "url": "/live-cricket-score/static/js/runtime-main.09726eea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live-cricket-score/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27b23a52df0c85503b9fe1b36595cb5a",
+    "url": "/live-cricket-score/static/media/vs.27b23a52.png"
+  }
+]);
